99af450e66b1e898ca6bad5df165f209b6e31ab6
necro <necro@DESKTOP-0D9AKVH>
2022-10-30 13:23:37 +0100

    Add project files.

---
+++ MagnethoreologicalFluid2DWithWalls/MagnethoreologicalFluid2DWithWalls.cpp
@@ -0,0 +1,29 @@
+﻿// MagnethoreologicalFluid2DWithWalls.cpp : Defines the entry point for the application.
+//
+
+#include "MagnethoreologicalFluid2DWithWalls.h"
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+	auto start = high_resolution_clock::now();
+	int particles = 400;
+	int boxLenght = 44;
+	double mason[14] = { 0.001 ,0.003, 0.004, 0.006, 0.009, 0.01, 0.04, 0.08, 0.1, 0.4, 0.6, 0.8, 1.0, 2.0 };
+	double times[14] = { 6000, 6000, 6000, 6000, 6000, 6000, 6000, 6000, 6000, 6000, 1000, 1000, 1000, 1000 };
+	int numbers = 14;
+	int repetitions = 5;
+	double deltaT = 0.001;
+
+	std::thread threads[5];
+
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<seconds>(stop - start);
+
+	cout << "Total time: "
+		<< duration.count() << " seconds" << endl;
+
+	return 0;
+}
---
+++ MagnethoreologicalFluid2DWithWalls/MagnethoreologicalFluid2DWithWalls.h
@@ -0,0 +1,23 @@
+﻿// MagnethoreologicalFluid2DWithWalls.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include <chrono>
+#include <random>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <sstream>
+#include <math.h>
+#include <CL/cl.hpp>
+#include <chrono>
+#include <thread>
+#include <cmath>
+
+// TODO: Reference additional headers your program requires here.
